b84c80d41d5cf923e911e6e0c18761308cd573fe
abelcg <abelcg86@gmail.com>
2021-08-12 11:33:50 -0300

    práctica javascript

---
+++ js/holaMundo.js
@@ -0,0 +1,31 @@
+//Este es un comentario de una sola linea
+
+/* Esto es un comentario 
+de multiples lineas */
+
+// mostrar mensaje en consola
+console.log('Hola mundo');
+
+// mostrar un mje en el html
+document.write("Este es mi primer msj");
+
+// mostrar un msj en  una ventana emergente
+// alert('Hola a todos')
+
+// crear una variable (var - let const), hoy en día ya no se usa var se usa let
+
+let comision = '16i';
+
+document.write(comision);
+
+document.write('<br>Comision: '+comision)
+
+// modicar el contenido de una variable
+
+comision = 'i';
+
+document.write('<br>Comision: '+comision)
+
+const anio = 2021;
+
+document.write('<br>Año: '+anio)
