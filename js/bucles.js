b51f816be351254a0793a69003ecafcaf0dda73b
abelcg <abelcg86@gmail.com>
2021-08-19 10:56:16 -0300

    se agrego archivo bucles

---
+++ js/bucles.js
@@ -0,0 +1,32 @@
+//while
+
+/*
+while(condicion logica){
+    el codigo a ejecutar
+    cambiar condicion logica
+}
+*/
+
+// let contador = 2;
+// while(contador <=20){
+//     document.write('Elemento '+ contador +'<br>');
+//    // contador++;
+//     contador = contador + 2;
+// }
+
+// do-while
+
+/* let cont = 50;
+do{
+    //codigo que quiero ejecutar varias veces
+    document.write('Contador '+ cont + '<br>');
+    //cambio la condicion logica
+    cont--
+}while(cont>=20) */
+
+/*for(crear la variable; condicion logica; incrementar o decremenatr la variable)
+...codigo a ajecutar muchas veces
+*/
+for(let i = 1; i<=20 ; i++ ){
+    document.write('Estructura for vuelta' + i + '<br>')
+}
