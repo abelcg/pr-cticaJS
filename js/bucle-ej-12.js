f342ff5bd896d474a2b470454bfdcc2834eeb6ac
abelcg <abelcg86@gmail.com>
2021-08-24 05:01:07 -0300

    ejercicio 12  de bucles

---
+++ js/bucle-ej-12.js
@@ -0,0 +1,5 @@
+//12- Realiza un script que genere un número aleatorio entre 1 y 99
+
+//Math.floor redondea al entero menor
+let num = Math.floor(Math.random()*99)+1; //Math.random() genera un alaeatorio entre 0 y 1, p. ej 0,345 
+document.write(num);
