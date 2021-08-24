790fe6129b3c9284f57d7392d667c3976b6cef14
abelcg <abelcg86@gmail.com>
2021-08-24 05:10:20 -0300

    ejercicio 13  de bucles

---
+++ js/bucle-ej-13.js
@@ -0,0 +1,8 @@
+// Ejercicios con String
+
+//13- Realiza un script que pida un texto y lo muestre en mayúsculas.
+
+
+let text = prompt('Ingrese un texto');
+
+document.write("TEXTO: " + text.toUpperCase());
