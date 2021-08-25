0f4eb2d410b16da95689bd6fe6c59e7fe1e891be
abelcg <abelcg86@gmail.com>
2021-08-25 20:45:28 -0300

    se agrego archivo funciones

---
+++ js/funciones.js
@@ -0,0 +1,14 @@
+// funciones sin parametros
+
+function holaMundo(){
+    //todo el codigo que quiero que tenga la funcion
+    document.write('Hola mundo');
+}
+
+//funciones con parametros
+function saludar(nombre, apellido){
+    document.write('<br>Hola mi nombre es: ' + nombre + ' y mi apellido es ' + apellido);
+}
+
+//ejecutar o invocar o llamar una funcion
+holaMundo();
