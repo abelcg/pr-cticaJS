544f7e9c29fe4e7b4f47347013915d056e0233bf
abelcg <abelcg86@gmail.com>
2021-09-06 20:13:02 -0300

    ejercicio 1  del TP de Objetos

---
+++ js/objetos-ej-1.js
@@ -0,0 +1,19 @@
+/* Crear objetos
+
+1- Crea un objeto llamado auto que tenga algunas características como el color, 
+marca, modelo y si está encendido o apagado. Crea los métodos necesarios para permitir encender y apagar el auto.
+ */
+
+let auto = {
+    color: 'rojo',
+    marca: 'Ford',
+    modelo: 'Mustang',
+
+    encendido: () => {
+        return true;
+    },
+
+    apagado: () => {
+        return false;
+    },
+}
